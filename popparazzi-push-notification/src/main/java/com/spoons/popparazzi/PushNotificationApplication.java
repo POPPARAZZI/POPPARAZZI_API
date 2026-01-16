d34a461864cfb95085f9219c8dff1ce2bc2b7fad
Inseon <somt1128@gmail.com>
2026-01-16 23:37:35 +0900

    첫 커밋

---
+++ popparazzi-push-notification/src/main/java/com/spoons/popparazzi/PushNotificationApplication.java
@@ -0,0 +1,11 @@
+package com.spoons.popparazzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushNotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PushNotificationApplication.class, args);
+    }
+}
